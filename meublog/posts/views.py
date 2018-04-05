b874086fb8837c6cf42e8498e4fff8ad31c96f29
beatrizreisdutra1000 <beatrizreisdutra@gmail.com>
2018-04-05 11:28:22 -0400

    fim aula 4

---
+++ meublog/posts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('Olá, estamos na view do app posts')
+# Create your views here.
